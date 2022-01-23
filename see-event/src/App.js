eb945307c3a02f2e9542eaab7bff2c6682e8ff82
BagusNugrohow <papaobobo@gmail.com>
2022-01-23 23:11:55 +0700

    mencoba routing di beberapa Page

--- see-event/src/App.js
+++ see-event/src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Home from "./pages/Home";
+import SearchEvents from "./pages/SearchEvents";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/search-events" element={<SearchEvents />} />
+      </Routes>
     </div>
   );
 }
