f7ef716d102042dccdf81039a3d3dfe2fa21276a
BagusNugrohow <papaobobo@gmail.com>
2022-01-23 08:33:16 +0700

    add files

---
+++ see-event/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return <div className={styles.Navbar}></div>;
+};
+
+export default Navbar;
