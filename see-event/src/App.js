9844ba4c59b991cf41b2057b34d68329c6eab647
BagusNugrohow <papaobobo@gmail.com>
2022-01-24 01:14:49 +0700

    Menambahkan component untuk My account page

--- see-event/src/App.js
+++ see-event/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+
 import Home from "./pages/Home";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <UserProfile />
     </div>
   );
 }
