ee687a3d763ebaa500d591b9aeeec7a23e46d3e1
BagusNugrohow <papaobobo@gmail.com>
2022-01-24 22:34:29 +0700

    fix app.js

--- see-event/src/App.js
+++ see-event/src/App.js
@@ -5,24 +5,13 @@ import "./App.css";
 import Home from "./pages/Home";
 import SearchEvents from "./pages/SearchEvents";
 
-import Login from "./pages/Login";
-
-import Footer from "./components/Footer";
-
-import Register from "./pages/Register";
-
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/search-events" element={<SearchEvents />} />
       </Routes>
-=======
-      {/* <Login /> */}
-      <Register />
->>>>>>> 56439adc5d9b08b133812321911f17cc45dc299e
     </div>
   );
 }
